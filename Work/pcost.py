442b01c3de6d4bb8d73da8cbbb3030ef0987b29d
Maddie Polk <maddiepolk@Maddies-Air.hsd1.tn.comcast.net>
2020-11-18 19:35:41 -0600

    Parts 1.6 and 1.7

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,30 @@
 # pcost.py
 #
 # Exercise 1.27
+
+# f = open('Data/portfolio.csv', 'rt')
+import csv
+import sys
+
+def portfolio_cost(filename):
+
+    try:
+        f = open(filename, 'rt')
+        rows = csv.reader(f)
+        headers = next(rows)
+        total = 0
+
+        for row in rows:
+            total += int(row[1]) * float(row[2])
+        
+        return total
+    except ValueError:
+        "Error message"
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+total = portfolio_cost(filename)
+print('Total cost:',total)
